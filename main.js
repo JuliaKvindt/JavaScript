a7140e5072672773e302990ad92101aef7aa27f4
July <july.chel90@gmail.com>
2022-12-09 14:01:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict"
+// Папка 3 задание 1
+// let password = 'nkjbhb';
+// if (password.length > 3 && (password.includes('-') || password.includes('_'))) {
+//     console.log('пароль надежный')
+// } else {
+//     console.log('пароль недостаточно надежный')
+// }
+
+//  first lesson
