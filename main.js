56882567153fa2f83d681d01d3ad0217510e0452
July <july.chel90@gmail.com>
2022-12-10 14:18:00 +0500

    Second lesson

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 //     console.log('пароль недостаточно надежный')
 // }
 
-//  first lesson
+//  first lesson, second lesson
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -18,12 +18,73 @@ const personalMovieDB = {
     geners: [],
     privat: false
 };
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+      
+      for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+                if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('ok');
+                } else {
+                    console.log('error');
+                    i--;
+                }              
+      }
+      if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+      } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+      } else {
+        console.log('Произошла ошибка');
+      }
       console.log(personalMovieDB);
+
+    //   console.log( NaN || 2 || undefined ); // 2
+    //   console.log( NaN && 2 && undefined ); // nan
+    //   console.log( 1 && 2 && 3 ); // 3
+    //   console.log( !1 && 2 || !3 ); // false
+    //   console.log( 25 || null && !3 );  //25    
+    //   console.log( NaN || null || !3 || undefined || 5); // 5
+    //   console.log( NaN || null && !3 && undefined || 5); // 5
+    //   console.log( 5 === 5 && 3 > 1 || 5); // true
+
+    // const hamburger = 3;
+    // const fries = 3;
+    // const cola = 0;
+    // const nuggets = 2;
+    
+    // if (hamburger === 3 && cola || fries === 3 && nuggets) {
+    //     console.log('Done!')
+    // }
+
+    // let hamburger;
+    // const fries = NaN;
+    // const cola = 0;
+    // const nuggets = 2;
+    
+    // if (hamburger || cola || fries === 3 || nuggets) {
+    // console.log('Done!')
+    // }
+
+    // let hamburger;
+    // const fries = NaN;
+    // const cola = 0;
+    // const nuggets = 2;
+    
+    // if (hamburger && cola || fries === 3 && nuggets) {
+    // console.log('Done!')
+    // }
+
+    // // пирамидка из *
+    // let result = '';
+    // const length = 7;
+    // for (let i = 1; i < length; i++) {
+    //     for (let j = 0; j < i; j++) {
+    //         result += '*';
+    //     }
+    //     result += '\n';
+    // }
+    // console.log(result);
+
